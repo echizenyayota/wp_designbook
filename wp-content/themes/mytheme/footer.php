d73aa18cb76d07776ac0b850eeae6651a87f915e
echizenyayota <virtualmisso30@gmail.com>
2016-11-02 17:01:33 +0900

    P53 コピーライト

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,6 +1,9 @@
 
 <footer>
-  Footer
+  <div class="copyright">
+    <p><?php bloginfo( 'description' ); ?></p>
+    <p>Copyright &copy; <?php bloginfo( 'name' ); ?></p>
+  </div>
 </footer>
 <?php get_footer(); ?>
 </body>
