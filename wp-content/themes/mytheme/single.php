eade169373f795cfe6945a1992b9e394dbab1344
echizenyayota <virtualmisso30@gmail.com>
2016-11-03 16:43:46 +0900

    P57 記事

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -2,7 +2,12 @@
 
 <div class="container">
   <div class="contents">
-    Contents
+    <?php if (have_posts()): while(have_posts()): the_post(); ?>
+      <article <?php post_class('kiji'); ?>>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+      </article>
+    <?php endwhile; endif; ?>
   </div>
 
   <div class="sub">
