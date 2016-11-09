283be652ad46f43eb8f90b2e953ea68d8e93361c
echizenyayota <virtualmisso30@gmail.com>
2016-11-09 11:38:58 +0900

    P132 更新日を表示する

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -5,9 +5,15 @@
     <?php if (have_posts()): while(have_posts()): the_post(); ?>
       <article <?php post_class('kiji'); ?>>
         <h1><?php the_title(); ?></h1>
-        <time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">
-          投稿日:<?php echo get_the_date(); ?>
-        </time>
+        <div class="kiji-data">
+          <time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">
+            投稿日:<?php echo get_the_date(); ?>
+          </time>
+            |
+          <time datetime="<?php echo get_the_modified_date( 'Y-m-d' ); ?>">
+            投稿日:<?php echo get_the_modified_date(); ?>
+          </time>
+        </div>
         <?php if (has_post_thumbnail()) : ?>
           <div class="catch">
             <?php the_post_thumbnail( 'large' ); ?>
