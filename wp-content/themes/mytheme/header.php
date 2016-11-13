783577d810d322aa5a52c0e62874c5b2010d88d7
echizenyayota <virtualmisso30@gmail.com>
2016-11-13 17:06:11 +0900

    P153 メタデータをOGPで記述する

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+<head prefix="og: http://ogp.me/ns#">
 <head>
   <meta charset="utf-8">
   <title>
@@ -10,6 +11,14 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="http://fonts.googleapis.com/earlyaccess/notosansjp.css">
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>"
+
+  <?php if ( is_single() ) : // 記事の個別ページ用のメタデータ?>
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="<?php the_title(); ?>">
+    <meta property="og:type" content="<?php the_permalink(); ?>">
+    <meta property="og:type" content="<?php echo wp_trim_words ($post->post_content, 100, '...'); ?>">
+  <?php endif; // 記事の個別ページ用のメタデータここまで?>
+
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
