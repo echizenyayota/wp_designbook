1b412fbd9f915b9cf970fb1cbc03fe8c1f659cac
echizenyayota <virtualmisso30@gmail.com>
2016-11-18 08:42:44 +0900

    P183独自のマークアップの選択肢を追加する

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -39,9 +39,31 @@ add_action('pre_post_update', 'clear_ytwrapper');
 // アイキャッチ画像の指定
 add_theme_support('post-thumbnails');
 
-// 編集画面の設定(h1を削除)
+// 編集画面の設定(h1を削除、補足情報と注意書きを追加)
 function editor_setting($init) {
   $init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;Preformatted=pre';
+  $style_formats = array(
+    array(
+      'title' => '補足情報',
+      'block' => 'div',
+      'classes' => 'point'
+    ),
+    array(
+      'title' => '注意書き',
+      'block' => 'div',
+      'classes' => 'attention'
+    )
+  );
+
+  $init['style_formats'] = json_encode($style_formats);
+
   return $init;
 }
 add_filter('tiny_mce_before_init', 'editor_setting');
+
+// スタイルメニューを有効化
+function add_stylemenu($buttons) {
+  array_splice($buttons, 1, 0, 'style');
+  return $buttons;
+}
+add_filter('mce_buttons_2', 'add_stylemenu');
