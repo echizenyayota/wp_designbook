7842179bb1055242daddf3ab212529451db9f661
echizenyayota <virtualmisso30@gmail.com>
2016-11-19 15:45:05 +0900

    P215 サムネイル画像とテキストを横に並べる

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -6,16 +6,19 @@
       <article <?php post_class('gaiyou'); ?>>
         <a href="<?php the_permalink(); ?>">
 
-            <img src="<?php echo mythumb('medium'); ?>">
+          <img src="<?php echo mythumb('medium'); ?>">
 
-          <h1><?php the_title(); ?></h1>
+          <div class="text">
+            <h1><?php the_title(); ?></h1>
 
-          <div class="kiji-date">
-            <i class="fa fa-pencil"></i>
-            <time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">投稿日:<?php echo get_the_date( 'Y-m-d' ); ?></time>
-          </div>
+            <div class="kiji-date">
+              <i class="fa fa-pencil"></i>
+              <time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">投稿日:<?php echo get_the_date( 'Y-m-d' ); ?></time>
+            </div>
 
-          <?php the_excerpt(); ?>
+            <?php the_excerpt(); ?>
+         </div>
+         
         </a>
       </article>
     <?php endwhile; endif; ?>
