e40ed207680db5c0ee08b068e45a10175d9ed770
echizenyayota <virtualmisso30@gmail.com>
2016-11-20 16:09:02 +0900

    P233 1件目の記事の概要のみを大きく表示する

--- wp-content/themes/mytheme/home.php
+++ wp-content/themes/mytheme/home.php
@@ -2,11 +2,14 @@
 
 <div class="container">
   <div class="contents">
-    <?php if (have_posts()): while(have_posts()): the_post(); ?>
-
+    <?php if (have_posts()): ?>
       <?php get_template_part('gaiyou', 'large'); ?>
+    <?php endif; ?>
 
+    <?php if (have_posts()): while(have_posts()): the_post(); ?>
+      <?php get_template_part('gaiyou', 'medium'); ?>
     <?php endwhile; endif; ?>
+
     <div class="pagination pagination-index">
       <?php echo paginate_links( array(
         'type' => 'list',
