3854f38ea65f582f65b87b3b62fecff9e7463717
echizenyayota <virtualmisso30@gmail.com>
2016-11-20 15:17:59 +0900

    P228 大サイズの概要のパーツを作成する

---
+++ wp-content/themes/mytheme/home.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="contents">
+    <?php if (have_posts()): while(have_posts()): the_post(); ?>
+
+      <?php get_template_part('gaiyou', 'large'); ?>
+
+    <?php endwhile; endif; ?>
+    <div class="pagination pagination-index">
+      <?php echo paginate_links( array(
+        'type' => 'list',
+        'end_size' => '2',
+        'mid_size' => '3',
+      )); ?>
+    </div>
+  </div>
+
+  <div class="sub">
+    <?php get_sidebar(); ?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
