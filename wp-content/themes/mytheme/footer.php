7a20a68e7de8b67e9b8475906c0a74058cdf43f6
echizenyayota <virtualmisso30@gmail.com>
2016-11-22 08:26:51 +0900

    P72 ヘッダーとフッターの中身を他のパーツに揃える

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,8 +1,9 @@
-
 <footer>
-  <div class="copyright">
-    <p><?php bloginfo( 'description' ); ?></p>
-    <p>Copyright &copy; <?php bloginfo( 'name' ); ?></p>
+  <div class="footer-inner">
+    <div class="copyright">
+      <p><?php bloginfo( 'description' ); ?></p>
+      <p>Copyright &copy; <?php bloginfo( 'name' ); ?></p>
+    </div>
   </div>
 </footer>
 <?php wp_footer(); ?>
