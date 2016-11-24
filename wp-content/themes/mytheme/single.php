8fd38174628455fc25e141d6892a5499099870a1
echizenyayota <virtualmisso30@gmail.com>
2016-11-24 10:53:52 +0900

    P262 ログイン中のユーザーによるアクセスを記録しない場合

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -90,9 +90,11 @@
 
 <?php get_footer(); ?>
 
-<?php // アクセス数の記録
-  $count_key = 'postviews';
-  $count = get_post_meta($post->ID, $count_key, true);
-  $count++;
-  update_post_meta($post->ID, $count_key, $count);
+<?php // アクセス数の記録(ログインしているユーザーは除外)
+  if (!is_user_logged_in() ) {
+    $count_key = 'postviews';
+    $count = get_post_meta($post->ID, $count_key, true);
+    $count++;
+    update_post_meta($post->ID, $count_key, $count);
+  }
 ?>
