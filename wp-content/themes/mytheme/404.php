8db56a8c525bc1dc42093f0dd745b9fc3b7b3605
echizenyayota <virtualmisso30@gmail.com>
2016-11-29 15:51:41 +0900

    768px以下のときはコンテンツ上部に320 * 50 のバナーを入れる（search.php 404.php)

--- wp-content/themes/mytheme/404.php
+++ wp-content/themes/mytheme/404.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 <div class="container">
+  <div class="google-mobile">
+    <div class="ad1">
+      <p><img src="" height="50" width=320></p>
+    </div>
+  <div>
   <div class="contents">
     <h2>404 not found!</h2>
       <p>申し訳ありません、現在表示する記事がありません。</p>
